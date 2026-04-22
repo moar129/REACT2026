a19419f586b51de7c382a4c55858237cc74ca7d8
Jens Hincke Friis <jef003@edu.zealand.dk>
2026-04-22 11:23:05 +0200

    fix: remove BrowserRouter import from App component as it is not used

--- MandatoryAssignment/src/App.tsx
+++ MandatoryAssignment/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar'
 import HomePage from './pages/homePage'
 import SearchPage from './pages/searchPage'
